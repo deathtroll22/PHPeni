dda8a8e2ab30a43117773c3fde90fb15c29b74f0
Nans ARNAUD <narnaud2023@campus-eni.fr>
2023-12-05 10:54:27 +0100

    creation du css et du html

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP Fil Rouge - Système de Gestion d'Articles</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Rechercher</a></li>
+            <li><a href="#">S'inscrire</a></li>
+            <li><a href="#">Se connecter</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section class="welcome">
+        <h1>Bienvenue sur notre Système de Gestion d'Articles</h1>
+        <p>Apprenez PHP en créant et en gérant des articles en ligne.</p>
+        <button>S'inscrire</button>
+    </section>
+    <section class="info">
+        <article>
+            <h2>Information</h2>
+            <p>Veuillez vous connecter pour utiliser l'application !</p>
+            <button>Se connecter</button>
+        </article>
+        <article>
+            <h2>À Propos</h2>
+            <p>Ce site vous permet d'apprendre le PHP en créant et en gérant des articles en ligne.</p>
+            <button>Se connecter</button>
+        </article>
+    </section>
+</main>
+<footer>
+    <p>ENI ECOLE - TP FIL ROUGE - Gestion des articles</p>
+</footer>
+</body>
+</html>
